45da5ec53b5e1e3986c9634f10e8ae169aa4347d
Nuno Costa <nuno@francodacosta.com>
2014-12-20 17:41:25 +0000

    bla

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,17 +7,20 @@
  * # MainCtrl
  * Controller of the potatoFlickrApp
  */
+
 angular.module('potatoFlickrApp')
   .controller('MainCtrl', function ($scope, $http) {
-    var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D\'http%3A%2F%2Fdailyjs.com%2Fatom.xml\'%20and%20itemPath%3D\'feed.entry\'&format=json&diagnostics=true&callback=JSON_CALLBACK';
+    // var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D\'http%3A%2F%2Fdailyjs.com%2Fatom.xml\'%20and%20itemPath%3D\'feed.entry\'&format=json&diagnostics=true&callback=JSON_CALLBACK';
+    var url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK'
 
+
+    var jsonFlickrFeed;
     $http.jsonp(url).
-      success(function(data, status, headers, config) {
+      success(jsonFlickrFeed = function(data, status, headers, config) {
         $scope.feed = {
-          title: 'DailyJS',
-          items: data.query.results.entry
+          title: data.title,
+          items: data.items
         };
-console.log($scope.feed);
       }).
       error(function(data, status, headers, config) {
         console.error('Error fetching feed:', data);
