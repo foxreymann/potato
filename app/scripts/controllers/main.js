7c8f5a7a0bda00104432c4e230d99b28917d3e05
tomaszjureczko <tomasz.jureczko@gmail.com>
2014-12-20 18:04:58 +0000

    clean up

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,12 +11,11 @@
 angular.module('potatoFlickrApp')
   .controller('MainCtrl', function ($scope, $http) {
     // var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D\'http%3A%2F%2Fdailyjs.com%2Fatom.xml\'%20and%20itemPath%3D\'feed.entry\'&format=json&diagnostics=true&callback=JSON_CALLBACK';
-    var url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK'
+    var url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK';
 
 
-    var jsonFlickrFeed;
     $http.jsonp(url).
-      success(jsonFlickrFeed = function(data, status, headers, config) {
+      success(function(data, status, headers, config) {
         $scope.feed = {
           title: data.title,
           items: data.items
