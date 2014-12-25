2067fda9124f7bd56dbfb85613710dbca8fc6218
tomaszjureczko <tomasz.jureczko@gmail.com>
2014-12-25 21:13:49 +0000

    mock.js

---
+++ test/mock/feed.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('mockedFeed', [])
+  .value('defaultJSON', {
+    {
+      "title": "Recent Uploads tagged potato",
+      "link": "https://www.flickr.com/photos/tags/potato/",
+      "description": "",
+      "modified": "2014-12-25T20:40:14Z",
+      "generator": "https://www.flickr.com/",
+      "items": [
+        {
+          "title": "Happy Thanksgiving 7",
+          "link": "https://www.flickr.com/photos/wanderingchopsticks/15917835378/",
+          "media": {"m":"https://farm9.staticflickr.com/8634/15917835378_261f4a64ab_m.jpg"},
+          "date_taken": "2014-11-29T16:23:36-08:00",
+          "description": " <p><a href=\"https://www.flickr.com/people/wanderingchopsticks/\">wanderingchopsticks<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/wanderingchopsticks/15917835378/\" title=\"Happy Thanksgiving 7\"><img src=\"https://farm9.staticflickr.com/8634/15917835378_261f4a64ab_m.jpg\" width=\"240\" height=\"160\" alt=\"Happy Thanksgiving 7\" /><\/a><\/p> ",
+          "published": "2014-12-25T20:40:14Z",
+          "author": "nobody@flickr.com (wanderingchopsticks)",
+          "author_id": "14741714@N00",
+          "tags": "thanksgiving pie purple sweet potato okinawan wanderingchopsticks"
+        },
+        {
+          "title": "Happy Thanksgiving 4",
+          "link": "https://www.flickr.com/photos/wanderingchopsticks/15919188059/",
+          "media": {"m":"https://farm8.staticflickr.com/7529/15919188059_56d8f7b468_m.jpg"},
+          "date_taken": "2014-11-27T16:10:56-08:00",
+          "description": " <p><a href=\"https://www.flickr.com/people/wanderingchopsticks/\">wanderingchopsticks<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/wanderingchopsticks/15919188059/\" title=\"Happy Thanksgiving 4\"><img src=\"https://farm8.staticflickr.com/7529/15919188059_56d8f7b468_m.jpg\" width=\"240\" height=\"160\" alt=\"Happy Thanksgiving 4\" /><\/a><\/p> ",
+          "published": "2014-12-25T20:40:12Z",
+          "author": "nobody@flickr.com (wanderingchopsticks)",
+          "author_id": "14741714@N00",
+          "tags": "thanksgiving pie purple sweet potato okinawan wanderingchopsticks"
+        },
+        {
+          "title": "Smoked Scallops, Rhubarb, and Purple Potatoes Salad",
+          "link": "https://www.flickr.com/photos/27110859@N06/16104467062/",
+          "media": {"m":"https://farm8.staticflickr.com/7503/16104467062_d2c496bb03_m.jpg"},
+          "date_taken": "2014-12-23T16:33:12-08:00",
+          "description": " <p><a href=\"https://www.flickr.com/people/27110859@N06/\">lyudavitaya<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/27110859@N06/16104467062/\" title=\"Smoked Scallops, Rhubarb, and Purple Potatoes Salad\"><img src=\"https://farm8.staticflickr.com/7503/16104467062_d2c496bb03_m.jpg\" width=\"240\" height=\"160\" alt=\"Smoked Scallops, Rhubarb, and Purple Potatoes Salad\" /><\/a><\/p> ",
+          "published": "2014-12-25T20:33:02Z",
+          "author": "nobody@flickr.com (lyudavitaya)",
+          "author_id": "27110859@N06",
+          "tags": "salad potato greens seafood scallop rhubarb smoked"
+        }
+      ]
+    }
+  });
