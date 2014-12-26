94b0ed15d9f2dcfbaf36913259c60c0146cd7170
tomaszjureczko <tomasz.jureczko@gmail.com>
2014-12-26 18:57:58 +0000

    config

--- app/scripts/configs/productionConfig.js
+++ app/scripts/configs/productionConfig.js
@@ -1,5 +1,6 @@
 angular.module('potatoFlickrApp')
   .constant('productionConfig', {
     feedUrl: 'https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK',
-    authorPageUrl: ' https://www.flickr.com/people/{0}'
+    authorPageUrl: ' https://www.flickr.com/people/{0}',
+    searchPageUrl: 'https://www.flickr.com/search?text={0}'
   })
