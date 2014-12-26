690bd02e3d22f1ea7e57d9b614c9f1ba848c6407
tomaszjureczko <tomasz.jureczko@gmail.com>
2014-12-26 16:52:24 +0000

    tests

--- test/spec/controllers/post.js
+++ test/spec/controllers/post.js
@@ -8,21 +8,22 @@ describe('Controller: PostCtrl', function () {
 
   // Initialize the controller and a mock scope
 
-  var runWithNoPostData = function() {
+  var customBeforeEach = function(setPostData) {
     inject(function ($controller, $rootScope) {
-/*
-      $rootScope.post = {
-        "title": "Happy Thanksgiving 7",
-        "link": "https://www.flickr.com/photos/wanderingchopsticks/15917835378/",
-        "media": {"m":"https://farm9.staticflickr.com/8634/15917835378_261f4a64ab_m.jpg"},
-        "date_taken": "2014-11-29T16:23:36-08:00",
-        "description": " <p><a href=\"https://www.flickr.com/people/wanderingchopsticks/\">wanderingchopsticks<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/wanderingchopsticks/15917835378/\" title=\"Happy Thanksgiving 7\"><img src=\"https://farm9.staticflickr.com/8634/15917835378_261f4a64ab_m.jpg\" width=\"240\" height=\"160\" alt=\"Happy Thanksgiving 7\" /><\/a><\/p> ",
-        "published": "2014-12-25T20:40:14Z",
-        "author": "nobody@flickr.com (wanderingchopsticks)",
-        "author_id": "14741714@N00",
-        "tags": "thanksgiving pie purple sweet potato okinawan wanderingchopsticks"
-      };
-*/
+      if(setPostData) {
+        $rootScope.post = {
+          "title": "Happy Thanksgiving 7",
+          "link": "https://www.flickr.com/photos/wanderingchopsticks/15917835378/",
+          "media": {"m":"https://farm9.staticflickr.com/8634/15917835378_261f4a64ab_m.jpg"},
+          "date_taken": "2014-11-29T16:23:36-08:00",
+          "description": " <p><a href=\"https://www.flickr.com/people/wanderingchopsticks/\">wanderingchopsticks<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/wanderingchopsticks/15917835378/\" title=\"Happy Thanksgiving 7\"><img src=\"https://farm9.staticflickr.com/8634/15917835378_261f4a64ab_m.jpg\" width=\"240\" height=\"160\" alt=\"Happy Thanksgiving 7\" /><\/a><\/p> ",
+          "published": "2014-12-25T20:40:14Z",
+          "author": "nobody@flickr.com (wanderingchopsticks)",
+          "author_id": "14741714@N00",
+          "tags": "thanksgiving pie purple sweet potato okinawan wanderingchopsticks"
+        };
+      }
+
       rootScope = $rootScope;
 
       scope = $rootScope.$new();
@@ -34,13 +35,14 @@ describe('Controller: PostCtrl', function () {
   };
 
   it('should redirect to / if post data is not available in rootScope', function () {
-    runWithNoPostData();
+    customBeforeEach(setPostData = false);
     inject(function($location) {
       expect($location.path()).toBe('/');
     });
   });
 
   it('should copy post data from rootScope to scope', function () {
+    customBeforeEach(setPostData = true);
     expect(scope.post.title).toBe('Happy Thanksgiving 7');
     expect(scope.post).toBe(rootScope.post);
   });
